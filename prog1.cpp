2ab34747a836f465858e9a509d1c8e1d51c630ce
Vibhuti00 <273vibhuti@gmail.com>
2025-11-20 15:13:48 +0530

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Time
+{
+    // FORMAT MODIFIERS
+public:
+    int h; // HOURS
+    int m; // MINUTES
+    int s; // SECONDS
+
+public:
+    // METHOD
+    void setSeconds(int total_seconds)
+    {
+        h = total_seconds / 3600;
+        m = (total_seconds % 3600) / 60;
+        s = total_seconds % 60;
+    }
+    void getTime()
+    {
+        cout << "HH:MM:SS => " << h << ":" << m << ":" << s << endl;
+    }
+};
+int main()
+{
+    int total_seconds;
+    cout << "Enter The Total No. Of Seconds : ";
+    cin >> total_seconds;
+    Time t;
+    t.setSeconds(total_seconds);
+    t.getTime();
+    return 0;
+}
---
+++ prog2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class Time
+{
+public:
+    int h, m, s;
+
+public:
+    void setTime(int hours, int minutes, int seconds)
+    {
+        h = hours;
+        m = minutes;
+        s = seconds;
+    }
+    void getTotal_seconds()
+    {
+        int total = h * 3600 + m * 60 + s;
+        cout << "Total seconds : " << total << endl;
+    }
+};
+int main()
+{
+    int h, m, s;
+    cout << "Enter Hours : ";
+    cin >> h;
+    cout << "Enter Minutes : ";
+    cin >> m;
+    cout << "Enter The Total No. Seconds : ";
+    cin >> s;
+    Time t;
+    t.setTime(h, m, s);
+    t.getTotal_seconds();
+    return 0;
+}
